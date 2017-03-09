a40a7b46dd8ad7b44345d195f44485848aabdc24
Ashish sah <Ashish sah>
2017-03-09 17:54:24 +0530

    Finalize the look of about-us content still to be added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
 $(document).ready(function() {
-    alert('hey i am Linked');
+    $('#load').addClass('hidden');
+    $('.slider').slider();
+    $('#ujjwala').hide();
+    $('#gourav').hide();
+    $('#animesh').hide();
+    $('#arya').hide();
+
+    $(window).scroll(function() {
+        //if you hard code, then use console
+        //.log to determine when you want the 
+        //nav bar to stick.  
+        console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 210) {
+            $('#nav').addClass('fix');
+        }
+        if ($(window).scrollTop() < 210) {
+            $('#nav').removeClass('fix');
+        }
+
+        if ($(window).scrollTop() > 280) {
+            $('#ujjwala').fadeIn(4000);
+        }
+        if ($(window).scrollTop() > 670) {
+            $('#gourav').fadeIn(4000);
+        }
+        if ($(window).scrollTop() > 1090) {
+            $('#animesh').fadeIn(4000);
+            $('#arya').fadeIn(4000);
+        }
+    });
+
+    $(document).ready(function() {
+        $('.parallax').parallax();
+    });
 });
